5b8c41485cef3a7f017af6dcbfa8041448e03b2d
Shinichi Kanai <shinichi.kanai@twelve-axis.com>
2024-11-04 19:30:51 +0900

    End of 13

---
+++ hooks/useQueryAvatar.ts
@@ -0,0 +1,23 @@
+import { useQuery } from 'react-query'
+import { supabase } from '@/utils/supabase'
+import { Profile } from '@/types'
+import { use } from 'react'
+
+export const useQueryAvatar = (userId: string | undefined) => {
+  const getAvatarUrl = async () => {
+    const { data, error } = await supabase
+      .from('profiles')
+      .select('avatar_url')
+      .eq('id', userId)
+      .single()
+
+    if (error) {
+      throw new Error(error.message)
+    }
+    return data
+  }
+  return useQuery<Profile, Error>({
+    queryKey: ['avatar_url', userId],
+    queryFn: getAvatarUrl,
+  })
+}
--- store/index.ts
+++ store/index.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 import { Session } from '@supabase/supabase-js'
-import { EditedNotice, EditedProfile } from '@/types'
+import { EditedNotice, EditedProfile, EditedPost } from '@/types'
 
 type State = {
   session: Session | null
@@ -13,6 +13,10 @@ type State = {
   editedNotice: EditedNotice
   updateEditedNotice: (payload: EditedNotice) => void
   resetEditedNotice: () => void
+
+  editedPost: EditedPost
+  updateEditedPost: (payload: EditedPost) => void
+  resetEditedPost: () => void
 }
 
 const useStore = create<State>((set) => ({
@@ -44,6 +48,18 @@ const useStore = create<State>((set) => ({
       },
     }),
   resetEditedNotice: () => set({ editedNotice: { id: '', content: '' } }),
+  editedPost: { id: '', title: '', post_url: '' },
+  updateEditedPost: (payload) =>
+    set({
+      editedPost: {
+        id: payload.id,
+        title: payload.title,
+        post_url: payload.post_url,
+      },
+    }),
+  resetEditedPost: () => {
+    set({ editedPost: { id: '', title: '', post_url: '' } })
+  },
 }))
 
 export default useStore
